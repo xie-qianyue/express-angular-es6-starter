8c58cde32c876724c96d93619479cb16256de803
Qianyue XIE <xie.qianyue@gmail.com>
2015-10-27 11:26:32 +0100

    move newTodo, todoList, todoFooter from common to components

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,14 +1,8 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
-import NewTodo from './newTodo/newTodo';
-import TodoFooter from './todoFooter/todoFooter';
-import TodoList from './todoList/todoList';
 
 let commonModule = angular.module('app.common', [
-    Navbar.name,
-    NewTodo.name,
-    TodoFooter.name,
-    TodoList.name
+    Navbar.name
 ]);
 
 export default commonModule;
--- client/app/components/components.js
+++ client/app/components/components.js
@@ -2,11 +2,17 @@ import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
 import Todo from './todo/todo';
+import NewTodo from './newTodo/newTodo';
+import TodoFooter from './todoFooter/todoFooter';
+import TodoList from './todoList/todoList';
 
 let componentModules = angular.module('components', [
     Home.name,
     About.name,
-    Todo.name
+    Todo.name,
+    NewTodo.name,
+    TodoFooter.name,
+    TodoList.name
 ]);
 
 export default componentModules;
