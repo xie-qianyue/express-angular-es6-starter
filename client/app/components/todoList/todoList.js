85fc133162453b553dc775b69f30b0be04856d13
Qianyue XIE <xie.qianyue@gmail.com>
2015-10-29 17:34:29 +0100

    add focus and escape directives

--- client/app/components/todoList/todoList.js
+++ client/app/components/todoList/todoList.js
@@ -3,6 +3,37 @@ import todoListComponent from './todoList.component';
 
 let todoListModule = angular.module('todoList', [])
 
-.directive('todoList', todoListComponent);
+.directive('todoList', todoListComponent)
+
+/**
+ * Directive that places focus on the element which it is applied to when the
+ * expression it binds to evaluates to true
+ */
+.directive('todoFocus', ($timeout) => {
+	return (scope, elem, attrs) => {
+		scope.$watch(attrs.todoFocus, (newVal) => {
+			if (newVal) {
+				$timeout(() => {
+					elem[0].focus();
+				}, 0, false);
+			}
+		});
+	};
+})
+
+/**
+ * Directive that executes an expression when the element it is applied to gets
+ * an `escape` keydown event.
+ */
+.directive('todoEscape', () => {
+	var ESCAPE_KEY = 27;
+	return (scope, elem, attrs) => {
+		elem.bind('keydown', (event) => {
+			if (event.keyCode === ESCAPE_KEY) {
+				scope.$apply(attrs.todoEscape);
+			}
+		});
+	};
+});
 
 export default todoListModule;
