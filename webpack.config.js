69cae34ae4bca7f224cf5ada3cf6964adfda6e57
XIE Qianyue <xie.qianyue@gmail.com>
2015-11-09 11:54:57 +0100

    Update webpack.config.js
    delete unused codes

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,10 @@ module.exports = {
   output: {
     filename: 'bundle.js'
   },
-  // resolve: {
-  //   // tells webpack to query these directories for modules
-  //   modulesDirectories: ["bower_components"]
-  // },  
   module: {
     loaders: [
       { test: /\.js$/, exclude: [/node_modules/, /bower_components/, /server/], loader: 'babel' },
       { test: /\.html$/, loader: 'raw' },
-      // { test: /\.styl$/, loader: 'style!css!stylus' },
-      // { test: /\.css$/, loader: 'style!css' }
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
